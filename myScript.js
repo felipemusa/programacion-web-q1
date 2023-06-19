3f54da54fdec1548159fd5a64969e8b6b31e9bb0
felipemusa <127269659+felipemusa@users.noreply.github.com>
2023-06-18 23:03:29 -0300

    cerca de terminar1

--- myScript.js
+++ myScript.js
@@ -1,21 +1,38 @@
 let total = 0;
 let cantidad=0;
-function sumarprecio(valorproducto) {
+
+function comprar(valorproducto) {
   total = total + valorproducto;
   document.getElementById("total-a-pagar").innerHTML =
      "$" + total ;
   cantidad+=1;
   document.getElementById('cantidad').innerHTML = cantidad;
   document.getElementById('no-productos').innerHTML=" ";
+  let contadorProducto = document.getElementById('contador-producto');
+  contadorProducto.innerHTML = parseInt(contadorProducto.innerHTML) + 1;
+}
 
+function descomprar(valorproducto) {
+  if (cantidad > 0) {
+    total = total - valorproducto;
+    document.getElementById("total-a-pagar").innerHTML = "$" + total;
+    cantidad -= 1;
+    document.getElementById('cantidad').innerHTML = cantidad;
+    document.getElementById('no-productos').innerHTML = " ";
+    let contadorProducto = document.getElementById('contador-producto');
+    contadorProducto.innerHTML = parseInt(contadorProducto.innerHTML) - 1;
     }
-function Borrar() {
+}
+
+function vaciar() {
   total = 0;
   document.getElementById("total-a-pagar").innerHTML = " $" + total;
   document.getElementById('no-productos').innerHTML="No hay productos en el carrito.";
   cantidad=0;
   document.getElementById('cantidad').innerHTML = cantidad;
-
+  let contadoresProductos = document.getElementsByClassName('contador-producto');
+  for (let i = 0; i < contadoresProductos.length; i++) {
+    contadoresProductos[i].innerHTML = "0";}
 }
 
 // hacer una funcion inversa a la de SUMAR PRECIO. y
