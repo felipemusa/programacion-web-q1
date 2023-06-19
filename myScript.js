7488276d44ae6a96a1d7b202724fa5e070b22771
felipemusa <127269659+felipemusa@users.noreply.github.com>
2023-06-19 01:30:15 -0300

    intento de captcha 1

--- myScript.js
+++ myScript.js
@@ -1,15 +1,13 @@
 let total = 0;
-let cantidad=0;
+let cantidad = 0;
 
 function comprar(valorproducto) {
   total = total + valorproducto;
-  document.getElementById("total-a-pagar").innerHTML =
-     "$" + total ;
-  cantidad+=1;
-  document.getElementById('cantidad').innerHTML = cantidad;
-  document.getElementById('no-productos').innerHTML=" ";
-  let contadorProducto = document.getElementById('contador-producto');
-  contadorProducto.innerHTML = parseInt(contadorProducto.innerHTML) + 1;
+  document.getElementById("total-a-pagar").innerHTML = "$" + total;
+  cantidad += 1;
+
+  document.getElementById("cantidad").innerHTML = cantidad;
+  document.getElementById("no-productos").innerHTML = " ";
 }
 
 function descomprar(valorproducto) {
@@ -17,23 +15,26 @@ function descomprar(valorproducto) {
     total = total - valorproducto;
     document.getElementById("total-a-pagar").innerHTML = "$" + total;
     cantidad -= 1;
-    document.getElementById('cantidad').innerHTML = cantidad;
-    document.getElementById('no-productos').innerHTML = " ";
-    let contadorProducto = document.getElementById('contador-producto');
-    contadorProducto.innerHTML = parseInt(contadorProducto.innerHTML) - 1;
-    }
+    document.getElementById("cantidad").innerHTML = cantidad;
+    document.getElementById("no-productos").innerHTML = " ";
+  }
 }
 
 function vaciar() {
   total = 0;
   document.getElementById("total-a-pagar").innerHTML = " $" + total;
-  document.getElementById('no-productos').innerHTML="No hay productos en el carrito.";
-  cantidad=0;
-  document.getElementById('cantidad').innerHTML = cantidad;
-  let contadoresProductos = document.getElementsByClassName('contador-producto');
-  for (let i = 0; i < contadoresProductos.length; i++) {
-    contadoresProductos[i].innerHTML = "0";}
+  document.getElementById("no-productos").innerHTML =
+    "No hay productos en el carrito.";
+  cantidad = 0;
+  document.getElementById("cantidad").innerHTML = cantidad;
 }
 
-// hacer una funcion inversa a la de SUMAR PRECIO. y
-// agregarla a un botón al lado de comprar 
+function validarCaptcha() {
+  var response = grecaptcha.getResponse();
+  if (response.length === 0) {
+    console.log("Por favor, completa el captcha.");
+    return false;
+  } else {
+    return true;
+  }
+}
