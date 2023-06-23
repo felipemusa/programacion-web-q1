d7b6e1e5caa8022253d8dfe543204a167d1d22f7
felipemusa <127269659+felipemusa@users.noreply.github.com>
2023-06-23 00:08:17 -0300

    boton de comprar
    motivación nocturna

--- myScript.js
+++ myScript.js
@@ -1,13 +1,22 @@
 let total = 0;
 let cantidad = 0;
+const comprarBtn = document.getElementById("comprarBtn");
+
+function actualizarEstadoComprar() {
+  if (cantidad === 0) {
+    comprarBtn.disabled = true;
+  } else {
+    comprarBtn.disabled = false;
+  }
+}
 
 function comprar(valorproducto) {
   total = total + valorproducto;
   document.getElementById("total-a-pagar").innerHTML = "$" + total;
   cantidad += 1;
-
   document.getElementById("cantidad").innerHTML = cantidad;
-  document.getElementById("no-productos").innerHTML = " ";
+  document.getElementById("no-productos").innerHTML = "";
+  actualizarEstadoComprar();
 }
 
 function descomprar(valorproducto) {
@@ -16,17 +25,19 @@ function descomprar(valorproducto) {
     document.getElementById("total-a-pagar").innerHTML = "$" + total;
     cantidad -= 1;
     document.getElementById("cantidad").innerHTML = cantidad;
-    document.getElementById("no-productos").innerHTML = " ";
+    document.getElementById("no-productos").innerHTML = "";
+    actualizarEstadoComprar();
   }
 }
 
 function vaciar() {
   total = 0;
-  document.getElementById("total-a-pagar").innerHTML = " $" + total;
+  document.getElementById("total-a-pagar").innerHTML = "$" + total;
   document.getElementById("no-productos").innerHTML =
     "No hay productos en el carrito.";
   cantidad = 0;
   document.getElementById("cantidad").innerHTML = cantidad;
+  actualizarEstadoComprar();
 }
 
 function validarCaptcha() {
@@ -38,3 +49,37 @@ function validarCaptcha() {
     return true;
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var finalizarBtn = document.getElementById("finalizarBtn");
+  var popup = document.getElementById("popup");
+  var closeBtn = document.getElementsByClassName("close")[0];
+  var termsCheckbox = document.getElementById("termsCheckbox");
+  var nombreInput = document.getElementById("nombre");
+  var apellidoInput = document.getElementById("apellido");
+
+  comprarBtn.addEventListener("click", function() {
+    var cantidadValor = parseInt(document.getElementById("cantidad").textContent);
+    if (cantidadValor !== 0) {
+      popup.style.display = "block";
+    }
+  });
+
+  closeBtn.addEventListener("click", function() {
+    popup.style.display = "none";
+  });
+
+  finalizarBtn.addEventListener("click", function() {
+    var nombre = nombreInput.value;
+    var apellido = apellidoInput.value;
+
+    if (nombre === "" || apellido === "" || !termsCheckbox.checked) {
+      alert("Por favor, complete todos los campos antes de finalizar la compra.");
+    } else {
+      alert("Gracias por su compra!");
+      popup.style.display = "none"; 
+    }
+  });
+
+  actualizarEstadoComprar();
+});
